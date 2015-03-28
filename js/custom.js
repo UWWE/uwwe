b9c45cb39cbc878c229b01d031f2cba7c7271622
Marc <lo.hungyui@gmail.com>
2015-03-28 02:06:14 -0400

    font fix
    Former-commit-id: acadd531caa03d29fe4ba280852a9c510423d5b4

--- js/custom.js
+++ js/custom.js
@@ -429,9 +429,9 @@ jQuery(function($){
 
 	
 	  jQuery(window).load(function() { // makes sure the whole site is loaded
-      ellipsis.stop();
-      $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(100).css({'overflow':'visible'});
+      // ellipsis.stop();
+      // $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      // $('body').delay(100).css({'overflow':'visible'});
     })
 
 
